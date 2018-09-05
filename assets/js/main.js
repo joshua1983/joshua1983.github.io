4f85e84f0aa24c53f858ff697f61307d19cd86ae
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2018-09-05 09:07:41 -0500

    Revision scroll y datos personales

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,36 @@ $(document).ready(function(){
     $("div.menu li").on('mouseout',function(){             
         $(this).find("div").removeClass(efectoItemMenu);
     });
+});
 
-    /*
-    especificar los altos de cada pagina para las opciones del menu
-    segun el alto de la pantalla visible
-    */
-})
+$("a.scroll-suave")
+.click(function(event){
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+            } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+            };
+            });
+        }
+    }
+});
