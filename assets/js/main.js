8a1f306cc88cdee42fcd85c267fe028f435b70a1
Josue <josue.barrios@gmail.com>
2018-09-04 22:59:26 -0500

    Revision de imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-    let efecto = 'bounceIn';
+    let efectoItemMenu = 'bounceIn';
     $("div.menu li").on('mouseover',function(){     
-        $(this).find("div").addClass(efecto);
+        $(this).find("div").addClass(efectoItemMenu);
     });
     $("div.menu li").on('mouseout',function(){             
-        $(this).find("div").removeClass(efecto);
+        $(this).find("div").removeClass(efectoItemMenu);
     });
+
+    /*
+    especificar los altos de cada pagina para las opciones del menu
+    segun el alto de la pantalla visible
+    */
 })
